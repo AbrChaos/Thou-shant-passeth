e6fe4a21d36fab5df1d100c533e8856a1c1dd8ca
AbrChaos <129905397+AbrChaos@users.noreply.github.com>
2023-05-07 20:40:13 +0300

    Add files via upload

--- face_detect.py
+++ face_detect.py
@@ -5,6 +5,7 @@ import pickle
 import numpy as np
 import pyautogui as pag
 import time
+import serial
 
 
 vid = cv2.VideoCapture(0)
@@ -12,6 +13,15 @@ vid = cv2.VideoCapture(0)
 workbook = oxl.load_workbook('Data.xlsx')
 xcl = workbook.get_sheet_by_name('Lapa1')
 Img_number = xcl["E2"].value
+
+arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
+
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
 
 i = 1
 
@@ -37,8 +47,13 @@ while(True):
 
         if(result == [True]):
             print(xcl["F"+str(i+4)].value)
-            pag.alert(text=xcl["F"+str(i+4)].value, title='', button='OK')
-            time.sleep(7)
+            #pag.alert(text=xcl["F"+str(i+4)].value, title='', button='OK')
+            write_read(xcl["F"+str(i+4)].value)
+            time.sleep(2)
+           
+            
+
+
         
         i = i+1
 
